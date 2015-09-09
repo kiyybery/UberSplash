3047bb8fe41fdbde3c10b35d53a521fc1e1a4736
xueyanan <xueyanan199227@gmail.com>
2015-09-09 18:06:17 +0800

    first commit

---
+++ app/src/main/java/com/kobe/ubersplash/FormView.java
@@ -0,0 +1,43 @@
+package com.kobe.ubersplash;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+
+public class FormView extends LinearLayout {
+
+    private EditText edit1, edit2;
+
+    public FormView(Context context) {
+        super(context);
+        loadView();
+    }
+
+    public FormView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        loadView();
+    }
+
+    public FormView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        loadView();
+    }
+
+    private void loadView(){
+        setOrientation(VERTICAL);
+        LayoutInflater.from(getContext()).inflate(R.layout.form_view, this);
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
+    }
+
+    @Override
+    public void setFocusable(boolean focusable) {
+        super.setFocusable(focusable);
+        edit1.setFocusable(focusable);
+        edit2.setFocusable(focusable);
+    }
+}
